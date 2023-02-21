52390832ea2433f278af91722cabf8b596cee56a
Patricia Casenas <pat.casenas@gmail.com>
2023-02-22 01:09:05 +0800

    update items

--- index.php
+++ index.php
@@ -4,22 +4,32 @@ include_once 'php/dbConfig.php';
 if(!empty($_GET['status'])){
     switch($_GET['status']){
         case 'succ':
-            $statusType = 'alert-success';
+            $statusType = 'Success';
             $statusMsg = 'Student data has been imported successfully.';
             break;
         case 'err':
-            $statusType = 'alert-danger';
+            $statusType = 'Error';
             $statusMsg = 'A problem has occurred, please try again.';
             break;
         case 'invalid_file':
-            $statusType = 'alert-danger';
+            $statusType = 'Error';
             $statusMsg = 'Please upload a valid CSV file.';
             break;
         default:
             $statusType = '';
             $statusMsg = '';
     }
-}?>
+}
+if(isset($_POST['truncate'])) {
+    $query = "TRUNCATE table studentinfo";
+
+    if (mysqli_multi_query($db, $query)) {
+      echo '<script>alert("Members have been successfully deleted!")</script>';
+    } else {
+      echo "Error!" . mysqli_error($db);
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -30,13 +40,14 @@ if(!empty($_GET['status'])){
 </head>
 
 <body>
+<div class="container">
     <!-- Display status message -->
+    <span id="alert">
     <?php if(!empty($statusMsg)){
-        echo "$statusType: ";
+        echo "<p style='font-weight:500;'>$statusType!&nbsp;</p>";
         echo $statusMsg;
-    } ?><br>
+    } ?><br></span>
 
-    <div class="container">
         <table class="students-table">
             <thead>
                 <tr>
@@ -44,13 +55,14 @@ if(!empty($_GET['status'])){
                     <th>First Name</th>
                     <th>Student Number</th>
                     <th>Program</th>
-                    <th>Update Details</th>
+                    <th>Section</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
             // Display rows
             $result = $db->query("SELECT * FROM studentinfo ORDER BY lastName ASC");
+
             if($result != false && $result->num_rows > 0){
                 while($row = $result->fetch_assoc()){
             ?>
@@ -59,6 +71,7 @@ if(!empty($_GET['status'])){
                     <td><?php echo $row['firstName']; ?></td>
                     <td><?php echo $row['studNum']; ?></td>
                     <td><?php echo $row['studProg']; ?></td>
+                    <td><?php echo $row['section']; ?></td>
                 </tr>
                 <?php } }else{ ?>
                 <tr>
@@ -67,12 +80,14 @@ if(!empty($_GET['status'])){
                 <?php } ?>
             </tbody>
         </table>
-    </div>
     <!-- Import link -->
     <div class="import-btn-cont">
-        <form action="php/importData.php" method="post" enctype="multipart/form-data">
+        <form action="php/importStud.php" method="post" enctype="multipart/form-data">
             <input type="file" name="file" />
             <input type="submit" class="import-btn" name="importSubmit" value="IMPORT">
+        </form>
+        <form method="post" onsubmit="return confirm('Danger! This action deletes all members of this table.')">
+            <input type="submit" value="Delete students" name="truncate">
         </form>
     </div>
     </div>
--- php/importStud.php
+++ php/importStud.php
@@ -1,50 +1,35 @@
 <!-- Imports students to their course -->
 <?php
-// Load the database configuration file
 include_once 'dbConfig.php';
-
 if(isset($_POST['importSubmit'])){
-    
     // Allowed mime types
     $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
     
     // Validate whether selected file is a CSV file
-    if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $csvMimes)){
-        
+    if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $csvMimes)){    
         // If the file is uploaded
-        if(is_uploaded_file($_FILES['file']['tmp_name'])){
+        if(is_uploaded_file($_FILES['file']['tmp_name'])){            
+        $csvFile = fopen($_FILES['file']['tmp_name'], 'r');
+        
+        // Skip the first line
+        fgetcsv($csvFile); 
+        // Parse data from CSV file line by line
+        while(($line = fgetcsv($csvFile)) !== FALSE){
+            // Get row data
+            $lastName   = $line[0];
+            $firstName  = $line[1];
+            $studNum  = $line[2];
+            $studProg = $line[3];
+            $section = $line[4];
             
-            // Open uploaded CSV file with read-only mode
-            $csvFile = fopen($_FILES['file']['tmp_name'], 'r');
+            $db->query("INSERT INTO studentinfo (lastName, firstName, studNum, studProg, section) VALUES ('".$lastName."', '".$firstName."', '".$studNum."', '".$studProg."', '".$section."')");    
+            $db->query("UPDATE studentinfo SET studProg = UPPER(studProg), section = UPPER(section)");
+        }
             
-            // Skip the first line
-            fgetcsv($csvFile);
+        // Close opened CSV file
+        fclose($csvFile);
             
-            // Parse data from CSV file line by line
-            while(($line = fgetcsv($csvFile)) !== FALSE){
-                // Get row data
-                $lastName   = $line[0];
-                $firstName  = $line[1];
-                $studNum  = $line[2];
-                $studProg = $line[3];
-                
-                // Check whether member already exists in the database with the same email
-                $prevQuery = "SELECT id FROM studentinfo WHERE studNum = '".$line[2]."'";
-                $prevResult = $db->query($prevQuery);
-                
-                if($prevResult->num_rows > 0){
-                    // Update member data in the database
-                    $db->query("UPDATE studentinfo SET lastName = '".$lastName."', firstName = '".$firstName."', studProg = '".$studProg."', modified = NOW() WHERE studNum = '".$studNum."'");
-                }else{
-                    // Insert member data in the database
-                    $db->query("INSERT INTO studentinfo (lastName, firstName, studNum, studProg) VALUES ('".$lastName."', '".$firstName."', '".$studNum."', '".$studProg."')");
-                }
-            }
-            
-            // Close opened CSV file
-            fclose($csvFile);
-            
-            $qstring = '?status=succ';
+        $qstring = '?status=succ';
         }else{
             $qstring = '?status=err';
         }
@@ -54,5 +39,5 @@ if(isset($_POST['importSubmit'])){
 }
 
 // Redirect to the listing page
-header("Location: ../importStud.php".$qstring);
+header("Location: ../index.php".$qstring);
 ?>
