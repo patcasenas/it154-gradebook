d2d165b99471c4c02d48b48068c39c4add3451d0
unknown <pat.casenas@gmail.com>
2023-06-06 20:45:58 +0800

    edits

---
+++ php/footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+    <img src="img/soit-white@1x.png" alt="School of Information Technology" id="footer-soit"/>
+    <span class="icons-footer">
+        <p class="footer-text"><em>&copy;&nbsp;iGradebook 2023</em></p>
+</span>
+</div>
